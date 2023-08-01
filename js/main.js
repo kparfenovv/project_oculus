b99a7c4640ce5a77541d7e8536c46dfa5c73f5a0
Kirill Parfenov <kparfenovv@gmail.com>
2023-08-01 21:10:00 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// В начале код выбирает все элементы с классом "tabs__btn-item" и "tabs__content-item" с помощью метода querySelectorAll. 
+// Эти элементы представляют собой кнопки вкладок и секции содержимого вкладок соответственно.
+
+// Затем для каждой кнопки вкладки прикрепляется обработчик событий "click" с помощью метода forEach. 
+// Это означает, что когда пользователь щелкает на кнопке вкладки, будет выполнена функция-обработчик "open".
+
+// Функция open определяется для обработки события "click". 
+// Она получает объект события (обычно обозначается как evt) в качестве своего параметра, 
+// который содержит информацию о событии, которое вызвало функцию.
+
+// Внутри функции open сначала извлекается значение атрибута "data-button" с помощью dataset.button из щелкнутой кнопки вкладки. 
+// Этот атрибут содержит соответствующий идентификатор секции содержимого вкладки, связанный с нажатой кнопкой.
+
+// Затем удаляются классы "tabs__btn-item--active" у всех кнопок вкладок и "tabs__content-item--active" у всех секций содержимого вкладок. 
+// Это гарантирует, что только одна кнопка вкладки и ее соответствующая секция содержимого будут активными в данный момент.
+
+// После удаления активных классов добавляется класс "tabs__btn-item--active" к щелкнутой кнопке вкладки. 
+// Этот класс отвечает за визуальное выделение активной кнопки вкладки.
+
+// Наконец, находится секция содержимого вкладки с идентификатором, соответствующим значению "data-button", 
+// и добавляется класс "tabs__content-item--active". Этот класс отвечает за отображение соответствующего содержимого вкладки. 
+
+// Код позволяет переключаться между вкладками, позволяя пользователю нажимать на разные кнопки вкладок, 
+// чтобы отобразить соответствующие секции с содержимым.
+
+'use strict'
+
+const tabItem = document.querySelectorAll ('.tabs__btn-item');
+const tabContent = document.querySelectorAll ('.tabs__content-item');
+
+tabItem.forEach(function(element){
+    element.addEventListener('click', open);
+});
+
+function open(evt){
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function(item){
+        item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+
+}
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+   menu.classList.toggle ('menu--active');
+});
